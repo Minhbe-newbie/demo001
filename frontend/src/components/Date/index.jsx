e346ff24ba69949b902a29438266504496aa947f
Your Name <65aminh2001@gmail.com>
2021-12-25 16:17:45 +0700

    first commit

---
+++ frontend/src/components/Date/index.jsx
@@ -0,0 +1,28 @@
+import "./style.scss";
+
+function dateFromDay(year, day) {
+    let newYear;
+    if(day > 365) {
+        newYear = year + 1;
+    } else {
+        newYear = year;
+    }
+    var date = new Date(newYear, 0); // initialize a date in `year-01-01`
+    return new Date(date.setDate(day % 365)); // add the number of days
+}
+
+const options = { 
+    year: "numeric", 
+    month: "long", 
+    day: "numeric" 
+}
+
+const DateComponent = (props) => {
+    return (
+        <div className="date">
+            December 13, 2021
+        </div>
+    )
+}
+
+export default DateComponent;
